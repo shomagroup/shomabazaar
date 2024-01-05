bb2683c0823889575eb55706e542fea0f2b8cf22
shomagroup <kcasana@shomagroup.com>
2024-01-05 11:30:20 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,3 +1,4 @@
+let url = window.location.href;
 // Resy popup
 $('[resy-pop]').on('click', function() {
     $('.body').addClass('no-scrollito');
@@ -28,4 +29,36 @@ $('[parking-info]').on('click', function() {
 });
 // Back Custom
 $('[backerito]').attr('onclick', 'history.back(-1)');
+
+
 //
+
+// === UTM TRACKER === //
+$.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
+    if (results == null) { return null; } else { return results[1] || 0; }
+}
+//
+var inTwoMins = new Date(new Date().getTime() + 2 * 60 * 1000);
+//cookie setter
+//
+if (!$.urlParam('utm_source') == null || !$.urlParam('utm_source') == "") {
+var source = $.urlParam('utm_source').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+Cookies.set('source', source, { expires: 30 });
+Cookies.set('utm_url', url, { expires: 30 });
+}
+//
+if (!$.urlParam('utm_medium') == null || !$.urlParam('utm_medium') == "") {
+var medium = $.urlParam('utm_medium').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+Cookies.set('medium', medium, { expires: 30 });
+}
+//
+if (!$.urlParam('utm_campaign') == null || !$.urlParam('utm_campaign') == "") {
+    var campaign = $.urlParam('utm_campaign').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('campaign', campaign, { expires: 30 });
+}
+//
+if (!$.urlParam('utm_term') == null || !$.urlParam('utm_term') == "") {
+    var term = $.urlParam('utm_term').split('&')[0].replace(/\+/g, ' ').replace(/%20/g, ' ');
+    Cookies.set('term', term, { expires: 30 });
+}
